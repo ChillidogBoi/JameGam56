37fc1172aed404867f01666d3fcf6244ca5b7c91
P C <pjcpjcp@Outlook.com>
2026-01-24 12:04:21 -0600

    Day 7 pt 2 Bugfix

--- exports/index.service.worker.js
+++ exports/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1769274195|2365979295';
+const CACHE_VERSION = '1769277841|6012393234';
 /** @type {string} */
 const CACHE_PREFIX = 'Jame Gam 56-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
